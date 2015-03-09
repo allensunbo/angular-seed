9e11a496b9df15f288e7282bf8437956803cb185
allensunbo <allensunbo@gmail.com>
2015-03-09 16:05:10 -0400

    put contact view

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proteusApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the proteusApp
+ */
+angular.module('proteusApp')
+  .controller('ContactCtrl', function ($scope) {
+
+  });
